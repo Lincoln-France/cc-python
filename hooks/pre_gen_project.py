22a6db760730ec23e986c6f8d217abb587045aee
ahm <antoine.hamon@mel.lincoln.fr>
2020-12-14 13:35:20 +0000

    fix flake8

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
 
     MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-    package_name = '{{ cookiecutter.package_name }}'
-    conda_env_name = '{{ cookiecutter.project_name.lower().replace(' ', '-') }}'
+    package_name = "{{ cookiecutter.package_name }}"
+    conda_env_name = "{{ cookiecutter.project_name.lower().replace(' ', '-') }}"
 
     if not re.match(MODULE_REGEX, package_name):
         print('ERROR: The package name (%s) is not a valid Python module name. Please do not use a - and use _ instead' % package_name)
